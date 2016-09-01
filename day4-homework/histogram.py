96f2d52b193baba09fa8b8400781517522f585b4
Greg Fuller <gfuller9@jhu.edu>
2016-09-01 18:32:28 -0400

    question 2

---
+++ day4-homework/histogram.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+usage: ./histogram.py <ctab file>
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+df = pd.read_table(sys.argv[1])
+df_fp = df["FPKM"]>0
+dflog = np.log10(df[df_fp]["FPKM"].values)
+print dflog
+
+plt.figure
+plt.hist(dflog, bins = 30)
+
+plt.xlabel("log FPKM")
+plt.ylabel("Counts")
+plt.title("Transcripts in SRR072893")
+plt.savefig("histogram.png")
+plt.close()
+
