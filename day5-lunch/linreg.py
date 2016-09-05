a26e24f2b5308e980ed5997a8827bfc1f7ced2b9
Greg Fuller <gfuller9@jhu.edu>
2016-09-05 19:54:51 -0400

    with comments

--- day5-lunch/linreg.py
+++ day5-lunch/linreg.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+Usage: ./linreg.py <bigwig output file (tab)> <ctab file>
+"""
 
 import numpy as np
 import statsmodels.api as sm
@@ -10,35 +13,41 @@ ctab = open(sys.argv[2])
 
 d1 = {}
 d2 = {}
-
+#enumerate through the ctab file 
 for i, line in enumerate(ctab):
+    #skip header
     if i == 0:
         continue
+    #split open lines and tab separate
     else:    
         fields = line.rstrip("\r\n").split("\t")
+        #convert fpkm values to floats and append to a dictionary
         fields[10]=float(fields[10])
         d1[fields[5]] = fields[10]
    
 
 
 
-
+#extract values from the tab files to the dictionary; average from tab and then fpkm from the ctab as values for key of txnscript name
 for line in data1:
     fields = line.rstrip("\r\n").split("\t")
     if fields[0] in d1:
         d2[fields[0]] = [float(fields[3]),d1[fields[0]]]
-
+#make lists for averages and the fpkm
 aver = []
 fpkm = []
-
+#append to those lists
 for item in d2:
     aver.append(d2[item][0])
     fpkm.append(d2[item][1])
+#convert data to arrays
 aver = np.asarray(aver)
 fpkm = np.asarray(fpkm)
-plt.figure()
-plt.scatter(aver,fpkm)
-plt.show()
+#figure options to see what the coverage looks like plotted against fpkm
+#plt.figure()
+#plt.scatter(aver,fpkm)
+#plt.show()
+#make the model with linear regression
 model = sm.OLS(aver,fpkm)
 results = model.fit()
 print results.summary()
--- day5-lunch/txn_start.py
+++ day5-lunch/txn_start.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
-
+"""
+Usage: ./txn_start.py <file>
+"""
 import sys
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
+
+import matplotlib.pyplot as plt
+#I need a list of chromosomes to be sure I can sort things not in chromosomes
 chromosomes = ["X", "Y", "2L", "2R", "3L","3R", "4"]
+#Open the file
 data = open(sys.argv[1])
 
 #print "chromosome" + "\t" + "start" +"\t"+ "end" + "\t"+ "t_name"
-
+#Enumerate through the ctab file
 for i, line in enumerate(data):
+    #skip header
     if i == 0:
         continue
+    #split up lines by tab
     fields = line.rstrip("\r\n").split("\t")
+    #ignore reads that don't map to chromosomes
     if fields[1] not in chromosomes:
         continue
-    
+    #if they're on the negative strand, we need to use the end position to define the promoter region
     if fields[2] == "-":
-
+        #adding 500 to either end of the end position and printing it out in the format for .bed file
+        #also making sure if there is a transcript w/ end right at the start of chromosome it is handled differently
         if int(fields[4]) < 500:
             print fields[1] + "\t" + str(int(fields[4])+500) + "\t" + str(0) + "\t" + fields[5]
             
             continue
-        else:        
+        else:   
+                 
             print fields[1] + "\t" + str(int(fields[4])+500) + "\t" + str(int(fields[4])-500) + "\t" + fields[5]
-           
+    #if on + strand, use the start position       
     elif fields[2]== "+":
         if int(fields[3]) < 500:
             print fields[1]+ "\t" + str(0) + "\t" + str(int(fields[3])) + "\t" + fields[5]
@@ -36,13 +44,5 @@ for i, line in enumerate(data):
         continue
         
 
-#for _, _, chro, strand, start, end, t_name, _, _, _, _, _, _ in df.itertuples():
-#   if strand == "-":
 
-#        d[chro] = (int(pd.read_table(sys.argv[1], index_col = "chr")["start"])-500, int(pd.read_table(sys.argv[1], index = "chr")["start"])+500,pd.read_table(sys.argv[1], index = "chr")["t_name"])
-#    else: 
-#        d[chro] = (int(pd.read_table(sys.argv[1], index_col = "chr")["end"])+500, int(pd.read_table(sys.argv[1], index = "chr")["end"])-500,pd.read_table(sys.argv[1], index = "chr")["t_name"])
-
-#df = pd.DataFrame( d )
-#df.to_csv (sys.stdout)
     
